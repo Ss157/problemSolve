109b911fe92e8d21b7359fa57862c52d70198e86
Ss157 <janely24@naver.com>
2022-07-01 21:11:38 +0900

    220701v1

--- baekjoon/2439.py
+++ baekjoon/2439.py
@@ -1,7 +1,3 @@
 N=int(input())
-for i in range(N):
-    for j in range(4,i,-1):
-        print(" ", end="")
-    for j in range(i+1):
-        print("*", end="")
-    print()
+for i in range(1,N+1):
+    print(" "*(N-i)+"*"*(i))
