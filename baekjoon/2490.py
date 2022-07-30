4d09f8df91051f87540b4774af8c8e7a39375b00
Ss157 <janely24@naver.com>
2022-07-30 13:29:49 +0900

    220730v0

--- baekjoon/2490.py
+++ baekjoon/2490.py
@@ -2,18 +2,18 @@ for j in range(3):
     numList=list(map(int,input().split()))
     cnt_0=0
     cnt_1=0
-    for i in range(4):
+    for i in numList:
         if i==0:
             cnt_0+=1
         else:
             cnt_1+=1
-    if cnt_0==1 and cnt_1==3:
+    if cnt_0==1:
         print("A")
-    elif cnt_0==2 and cnt_1==2:
+    elif cnt_0==2:
         print("B")
-    elif cnt_0==3 and cnt_1==1:
+    elif cnt_0==3:
         print("C")
-    elif cnt_0==4 and cnt_1==0:
+    elif cnt_0==4:
         print("D")
-    elif cnt_0==0 and cnt_1==4:
+    else:
         print("E")
