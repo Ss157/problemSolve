d4ad4adbf97f9436b57c8fa725108b469b52f244
Ss157 <janely24@naver.com>
2022-08-01 21:00:30 +0900

    220801v2

---
+++ baekjoon/6502.py
@@ -0,0 +1,12 @@
+cnt=0
+while True:
+    length=input()
+    cnt+=1
+    if length=="0":
+        break
+    r,w,l=length.split()
+    diagonal=(int(w)**2+int(l)**2)**(1/2)
+    if int(r)*2>diagonal:
+        print("Pizza %d fits on the table."%cnt)
+    else:
+        print("Pizza %d does not fit on the table."%cnt)
