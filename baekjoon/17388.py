d57a889fd38ce3444ea9e7e579e4d3092941d5a3
Ss157 <janely24@naver.com>
2022-08-04 22:25:06 +0900

    220804v3

---
+++ baekjoon/17388.py
@@ -0,0 +1,9 @@
+S,K,H=map(int,input().split())
+if S+K+H>=100:
+    print("OK")
+elif S<K and S<H:
+    print("Soongsil")
+elif K<S and K<H:
+    print("Korea")
+else:
+    print("Hanyang")
--- baekjoon/9085.py
+++
@@ -1,4 +0,0 @@
-for i in range(int(input())):
-    N=int(input())
-    numList=list(map(int,input().split()))
-    print(sum(numList))
