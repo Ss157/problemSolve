90e58bdf41980506c37b740360ed2e15ee2c5659
Ss157 <janely24@naver.com>
2022-09-01 22:14:41 +0900

    220901v0

---
+++ baekjoon/13985.py
@@ -0,0 +1,5 @@
+str_1=list(map(str,input().split()))
+if (int(str_1[0])+int(str_1[2]))==int(str_1[4]):
+    print("YES")
+else:
+    print("NO")
--- baekjoon/16395.py
+++ baekjoon/16395.py
@@ -1,6 +1,3 @@
-import imp
-
-
 import math
 n,k=map(int,input().split())
 print(math.comb(n-1,k-1))
