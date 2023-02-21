76e3a6fdc4ca230a578aec254d377c6ef681b0e7
Ss157 <janely24@naver.com>
2023-02-21 13:59:57 +0900

    230221v1

---
+++ ITGoPython/Ch.07/ex02.py
@@ -0,0 +1,14 @@
+#문자열 전달에 대한 실습
+
+def change(string):
+    string+="행복합니다."
+    print("change()내의 string 값 : ",string)
+    print("change()내의 string의 주소(id) : ",id(string))
+    
+msg="안녕하세요 저는 "
+print("호출 전 msg 값 : " + msg)
+print("msg의 주소(id) : ",id(msg))
+change(msg)
+print("호출 후 msg 값 : " + msg)
+print("msg의 주소(id) : ",id(msg))
+    
