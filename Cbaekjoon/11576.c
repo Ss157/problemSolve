e94417c104df47237722e5764d3a7b995e23a1c6
Ss157 <janely24@naver.com>
2023-07-30 23:10:19 +0900

    230730v1

---
+++ Cbaekjoon/11576.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+void ten_to_B(int C, int B)
+{
+	if (C != 0)
+	{
+		ten_to_B(C / B, B);
+		printf("%d ", C % B);
+	}
+}
+
+int main(void){
+    int future_base, present_base, base_10 = 0, digit;
+    scanf("%d %d", &future_base, &present_base);
+    scanf("%d", &digit);
+    
+    int cnt = 0;
+    int num;
+    while(digit--){
+        scanf("%d", &num);
+        base_10 += num * pow(future_base, digit);
+    }
+    
+    ten_to_B(base_10, present_base);
+}
