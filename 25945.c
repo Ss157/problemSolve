bc261e370037ed54bb98ad9777f1c214c86eb1b6
Ss157 <janely24@naver.com>
2023-09-28 23:36:19 +0900

    230928v1

--- 25945.c
+++ 25945.c
@@ -9,26 +9,21 @@ int main(void){
         scanf("%d", &arr[i]);
         total_container += arr[i];
     }
-    int namagi = total_container % n;
-    int ahrt = total_container / n;
-    
-    int cnt = 0;
+    int average = total_container / n;
+    int cnt_over = 0;
+    int cnt_under = 0;
     for(int i = 0; i < n; i++){
-        if(arr[i] > ahrt + 1){
-            cnt += (arr[i] - ahrt + 1) * 1;
-            arr[i] = arr[i] - ahrt + 1;
+        if(arr[i] > average + 1){
+            cnt_over += arr[i] - average - 1;
         }
-        else if(arr[i] < ahrt){
-            if
-            arr[i] = arr[i] + ahrt;
+        else if(arr[i] < average){
+            cnt_under += average - arr[i];
         }
     }
-
-    for(int i = 0; i < n; i++){
-        if(arr[i] < ahrt - 1){
-            cnt += 1;
-        }
+    if(cnt_over >= cnt_under){
+        printf("%d", cnt_over);
     }
-
-    printf("%d", cnt);
+    else{
+        printf("%d", cnt_under);
+    }
 }
