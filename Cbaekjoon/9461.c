2c2eee27597a2c1586030817872b191f4d0d216c
Ss157 <janely24@naver.com>
2024-01-11 22:13:26 +0900

    240111v0

---
+++ Cbaekjoon/9461.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+    long long T, N, arr[100] = {1, 1, 1, 2, 2};
+    scanf("%lld", &T);
+    while(T--){
+        scanf("%lld", &N);
+        for(int i = 5; i < N; i++){
+            arr[i] = arr[i - 1] + arr[i - 5];
+        }
+        printf("%lld\n", arr[N - 1]);
+    }
+}
+//2 6 12   3 16
