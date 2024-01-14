91c19a3fbf3c84ec5d6626364b1295443d686961
Ss157 <janely24@naver.com>
2024-01-14 23:41:42 +0900

    240114v0

--- Cbaekjoon/1966.c
+++ Cbaekjoon/1966.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 
-//https://www.acmicpc.net/problem/1966
-//그냥 우선순위를 보면서 뒤로 계속 넘기면
-//되는 것 아닌가? 그냥 정직하게.
 int main(void){
+    int T, N, M, cnt, idx, arr[100] = {0};
+    scanf("%d", &T);
 
+    while(T--){
+        cnt = 1, idx = 0;
+        scanf("%d %d", &N, &M);
+        for(int i = 0; i < N; i++){
+            scanf("%d", &arr[i]);
+        }
+        while(1){
+            int max = 0;
+            
+            for(int j = 0; j < N; j++){
+                if(max < arr[j]){
+                    max = arr[j];
+                }
+            }
+            while (arr[idx] != max){
+                idx = (idx+1) % N;
+            }
+            if(idx == M){
+                break;
+            }
+            cnt++;
+            arr[idx] = 0;
+        }
+        printf("%d\n", cnt);
+    }
 }
