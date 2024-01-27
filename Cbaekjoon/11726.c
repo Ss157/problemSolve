715679126e1108d012a17262ab09cc720357e287
Ss157 <janely24@naver.com>
2024-01-27 22:32:29 +0900

    240127v1

---
+++ Cbaekjoon/11726.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int N;
+int arr[1000];
+int main(void){
+    scanf("%d", &N);
+    arr[0] = 1;
+    arr[1] = 2;
+    for (int i = 2; i < N; i++)
+    {
+        arr[i] = (arr[i - 1] + arr[i - 2]) % 10007;
+    }
+    printf("%d", arr[N - 1] % 10007);
+}
